7c7ef143bc4dbbc7f65353b2f3aab8b7d390283c
Luthfi <vlootfie@gmail.com>
2018-06-20 23:40:47 +0700

    register People component into parent component to have routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import store from './store';
-import logo from './logo.svg';
 import './App.css';
+import People from './components/People';
 
 class App extends Component {
   render() {
     return (
       <Provider store = { store }>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/' component = { People }/>
+          </Switch>
+        </BrowserRouter>
       </Provider> 
     );
   }
