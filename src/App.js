7fef844a0804479ec1a7bb8e1bba6977f2512190
Luthfi <vlootfie@gmail.com>
2018-06-21 08:59:22 +0700

    implement semantic-ui for component styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import store from './store';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
 import People from './components/People';
 import Person from './components/Person';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <Provider store = { store }>
-        <BrowserRouter>
-          <Switch>
-            <Route path='/:id' component = { Person }/>
-            <Route path='/' component = { People }/>
-          </Switch>
-        </BrowserRouter>
+        <div>
+          <BrowserRouter>
+            <div>
+              <Navbar/>
+              <Switch>
+                <Route path='/:id' component = { Person }/>
+                <Route path='/' component = { People }/>
+              </Switch>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider> 
     );
   }
